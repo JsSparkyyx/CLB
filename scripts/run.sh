00df974ba82f0bfb96e749a0f7cdd0327500114e
JsSparkYyx <754773895@qq.com>
2023-03-01 09:37:59 +0000

    update

--- scripts/run.sh
+++
@@ -1,4 +0,0 @@
-for seed in {1..5..1}
-do
-    python main.py --method EWC --seed $seed
-done
--- scripts/run_cifar100.sh
+++ scripts/run_cifar100.sh
@@ -1,4 +1,4 @@
-for seed in {1..5..1}
+for seed in {1..10..1}
 do
     python main.py --dataset CIFAR100 --method EWC --seed $seed --train_batch_size 256
 done
